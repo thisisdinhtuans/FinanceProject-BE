0556aa2d8e826b4a619f2f60d95583df1f26cf93
Dinh Tuans <dinhmanhtuans89@gmail.com>
2024-01-29 21:48:59 +0700

    User Generated Content

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68644a4ddf21eb6c01937ba91588a4d5b3fb3fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97d5a7d2f0ddb1b756d2e11942d444569f6ef11b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
